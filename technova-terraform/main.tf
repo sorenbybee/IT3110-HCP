f56b0c9377d94918efb6670b21a74f27ae51cc55
sorenbybee <d00462732@utahtech.edu>
2025-03-19 11:15:50 -0600

    removed local creds

--- technova-terraform/main.tf
+++ technova-terraform/main.tf
@@ -2,14 +2,13 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.45.0"
+      version = "5.91.0"
     }
   }
 }
 
 provider "aws" {
   region                   = var.aws_region
-  shared_credentials_files = ["~/.aws/credentials"]
 }
 
 # VPC Module
